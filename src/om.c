0fa553a9119d2b0cad1899230b87b003c4c5d972
xiao <2592509183@qq.com>
2022-01-13 09:57:02 +0800

    修复某些情况下编译错误

--- src/om.c
+++ src/om.c
@@ -10,6 +10,10 @@ om_status_t om_init() {
 }
 
 om_status_t om_deinit() {
-  return om_msg_deinit();
-  om_log_deinit();
+  om_status_t res = OM_OK;
+  res += om_msg_init();
+#if OM_LOG_OUTPUT
+  res += om_msg_deinit();
+#endif
+  return res;
 }
