04ccc44ed3067b08f45cbb9ff5a14cc22a4fb5e1
xiao <2592509183@qq.com>
2023-04-20 00:11:01 +0800

    添加crc校验

---
+++ src/comp/om_crc.h
@@ -0,0 +1,14 @@
+#ifndef __OM_CRC_H__
+#define __OM_CRC_H__
+
+#include "om_def.h"
+
+uint8_t om_crc8_calc(const uint8_t* buf, size_t len);
+
+bool om_crc8_verify(const uint8_t* buf, size_t len);
+
+uint32_t om_crc32_calc(const uint8_t* buf, size_t len);
+
+bool om_crc32_verify(const uint8_t* buf, size_t len);
+
+#endif
